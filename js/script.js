57037341703f575de2a415d65ae9d0c8726a1f16
MrArcano <salvatore-dario@hotmail.it>
2023-10-06 14:37:53 +0200

    [add]: email stamp

--- js/script.js
+++ js/script.js
@@ -7,21 +7,25 @@ createApp({
     }
   },
   methods: {
-    getAPI(){
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((response) => {
-          // handle success
-          console.log(response);
-        })
-        .catch((error) => {
-          // handle error
-          console.log("ciao");
-          console.log(error);
-        });
+    getAPI(items){
+      for (let index = 0; index < items; index++) {
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then((response) => {
+            // handle success
+            console.log(response.data.response);
+            this.emailArray.push(response.data.response);
+          })
+          .catch((error) => {
+            // handle error
+            console.log(error);
+          });
+        
+      }
     }
   },
   mounted() {
-    this.getAPI();
+    this.getAPI(10);
   },
 
 }).mount("#app");
